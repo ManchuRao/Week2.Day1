337404f7edc4a94ecf75ee718d5360d9e5df0e0a
ManchuRao <112795623+ManchuRao@users.noreply.github.com>
2022-09-16 15:16:15 +0530

    Add files via upload

---
+++ CreateLead2.java
@@ -0,0 +1,70 @@
+package sep.week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+     //webdrive set up
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		
+		//launch browser link
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		//maximize
+		
+		driver.manage().window().maximize();
+		
+		//user details
+		
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		//click crm/sfa
+		
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+		//click leads
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("ABC");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Manchu");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("P");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Rao");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("selenium testing");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("abc@gmz.com");
+		
+		//selecting by visible text
+		WebElement statedrp  =driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select drp1 = new Select(statedrp);
+		drp1.selectByVisibleText("New York");
+		driver.findElement(By.name("submitButton")).click();
+		
+		//selecting title
+		
+		String title = driver.getTitle();
+		System.out.println("The tilte of the resulting page is:" +title);
+		
+		Thread.sleep(2000);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
